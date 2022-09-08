67c44b3324c56cbedbf16a00b0924d2a4e130d39
JuanCeja <jmc347ls2@yahoo.com>
2022-09-07 19:34:06 -0700

    created directory component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CategoryItem from "../category-item/category-item.component";
+
+import './categories.styles.scss'
+
+const Directory = ({categories}) => {
+
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
