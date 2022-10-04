b90f98cad4dab7697166c9c57c62ea693cc01139
JuanCeja <jmc347ls2@yahoo.com>
2022-10-03 20:34:12 -0700

    finished creating user content

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   }
 
   return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -48,4 +48,21 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot)
   console.log(userSnapshot.exists())
-}
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email, 
+        createdAt,
+      });
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
