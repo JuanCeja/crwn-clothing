d3837651c2e8f340b9b73feafc638b74f4586d42
JuanCeja <jmc347ls2@yahoo.com>
2022-10-05 20:22:07 -0700

    created sign in component with google login

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  // we destructor off these values so we can use them later in our code
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      resetFormFields();
+    } catch (error) {}
+  };
+
+  // this function will take our input whenever the text changes in our input fields
+  const handleChange = (event) => {
+    // were going to utilize this name to tell our set state which of the fields to update
+    const { name, value } = event.target;
+
+    // we spread all of the fields and then were only going to update the appropriate field by using these square brackets inside of our object notation.
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        {/* on our input we want to name them the same as the name of the actual attribute that were trying to update */}
+        {/* we also set our value to our variable so changes are circular. like this the value from the state is the value thats shown in the input */}
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button onClick={signInWithGoogle} buttonType="google">
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
