7bd11daaac2a90de3bd766999049af8b26cfbc9b
JuanCeja <jmc347ls2@yahoo.com>
2022-10-26 13:40:13 -0700

    completed product card component

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -3,12 +3,12 @@ import { createContext, useState } from "react";
 import PRODUCTS from "../shop-data.json";
 
 export const ProductsContext = createContext({
-    products: [],
+  products: [],
 });
 
-export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
-    const value = { products };
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  const value = { products };
 
   return <ProductsContext.Provider value={value}> {children} </ProductsContext.Provider>;
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useContext } from "react";
 
+import './shop.styles.scss';
+
 import { ProductsContext } from "../../contexts/products.context";
+
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <div>
-      {products.map(({id, name}) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
