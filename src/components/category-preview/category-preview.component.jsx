5397277f60f8db534030db9cbf588061a17d2b0a
JuanCeja <jmc347ls2@yahoo.com>
2022-11-23 20:23:28 -0800

    converted to styled components

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,27 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
 import ProductCard from '../product-card/product-card.component';
 
-import './category-preview.styles.scss';
+import { CategoryPreviewContainer, Title, Preview } from './category-preview.styles';
 
 const CategoryPreview = ({ title, products }) => {
     return (
-        <div className='category-preview-container'>
+        <CategoryPreviewContainer>
             <h2>
-                <Link to={title} className='title'>
-                    {title.toUpperCase()}
-                </Link>
+                <Title to={title}>{title.toUpperCase()}</Title>
             </h2>
-            <div className='preview'>
-                {
-                    products
-                        .filter((_, idx) => idx < 4)
-                        .map((product) =>
-                            <ProductCard key={product.id} product={product} />)
-                }
-            </div>
-        </div>
+            <Preview>
+                {products
+                    .filter((_, idx) => idx < 4)
+                    .map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+            </Preview>
+        </CategoryPreviewContainer>
     )
 }
 
