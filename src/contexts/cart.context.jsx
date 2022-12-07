c6bd549034b931f566701662ee5a40fa7cb64a3d
JuanCeja <jmc347ls2@yahoo.com>
2022-12-06 18:21:52 -0800

    changes in cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useReducer } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
 
