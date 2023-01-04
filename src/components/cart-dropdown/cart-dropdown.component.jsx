a0f2761e810e68a8258ddb8f04f94e75ecf2f33d
JuanCeja <jmc347ls2@yahoo.com>
2023-01-03 18:44:30 -0800

    created cart actions,reducers,types,and selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { CartContext } from '../../contexts/cart.context';
-
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
