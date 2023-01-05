da013361256b23e53848a8a7b458e3f0a19d7995
JuanCeja <jmc347ls2@yahoo.com>
2023-01-04 18:26:23 -0800

    added redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -11,8 +13,18 @@ const loggerMiddleWare = (store) => (next) => (action) => {
     next(action);
 };
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
