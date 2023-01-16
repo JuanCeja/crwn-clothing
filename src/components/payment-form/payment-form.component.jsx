0c51dab8ea1c05a0f65d249b64895b46d6afb2bf
JuanCeja <jmc347ls2@yahoo.com>
2023-01-16 13:44:17 -0800

    setting up my payment form for stripe

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+import { PaymentFormContainer, FormContainer } from './payment-form.styles';
+import { async } from '@firebase/util';
+
+export const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if(!stripe || !elements) {
+            return;
+        }
+
+        
+        
+    }
+
+    return (
+        <PaymentFormContainer>
+            <FormContainer>
+            <h2>Credit Card Payment: </h2>
+                <CardElement />
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay Now </Button>
+            </FormContainer>
+        </PaymentFormContainer>
+    )
+}
+
+export default PaymentForm;
