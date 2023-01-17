8f942961ddcee71538d33988f96a88c73f915f43
JuanCeja <jmc347ls2@yahoo.com>
2023-01-17 15:08:04 -0800

    edited store

--- src/store/store.js
+++ src/store/store.js
@@ -11,20 +11,31 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
-}
+    whitelist: ['cart'],
+};
 
 const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(Boolean);
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger,
+    sagaMiddleware,
+].filter(Boolean);
 
-const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancer =
+    (process.env.NODE_ENV !== 'production' &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const store = createStore(
+    persistedReducer,
+    undefined,
+    composedEnhancers
+);
 
 sagaMiddleware.run(rootSaga);
 
